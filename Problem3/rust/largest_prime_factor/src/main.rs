426237c1d69d99953a29167e70c7d2cecccdb474
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-12-22 11:22:53 +1300

    3: Rather than find primes, just factorise number

--- Problem3/rust/largest_prime_factor/src/main.rs
+++ Problem3/rust/largest_prime_factor/src/main.rs
@@ -1,48 +1,26 @@
-extern crate bit_vec;
-
-use bit_vec::BitVec;
-
-fn sieve_of_eratosthenes(bound : usize) -> Vec<usize> {
-    let mut is_prime = BitVec::from_elem(bound, true);
-
-    // 0 and 1 are not prime
-    is_prime.set(0, false);
-    is_prime.set(1, false);
-
-    for i in 2..1 + (bound as f64).sqrt() as usize {
-        if is_prime[i] {
-            for j in i.. {
-                if i * j >= bound {
-                    break;
-                }
-                is_prime.set(i * j, false);
+fn find_greatest_prime_factor(number : usize) -> Option<usize> {
+    if number < 2 {
+        None
+    } else {
+        let mut factors = Vec::new();
+        let mut num = number;
+        let mut divisor = 2;
+        while num > 1 {
+            while num % divisor == 0 {
+                factors.push(divisor);
+                num /= divisor;
+            }
+            divisor += 1;
+            if divisor * divisor > num {
+                factors.push(num);
+                break;
             }
         }
-    }
-    let mut primes = Vec::new();
-    for (i, elem) in is_prime.iter().enumerate() {
-        if elem {
-            primes.push(i);
-        }
-    }
-    primes
-}
-
-fn find_greatest_prime_factor(number : usize) -> Option<usize> {
-    // Greatest factor can be half the numebr
-    for prime in sieve_of_eratosthenes(number / 2).into_iter().rev() {
-        if number % prime == 0 {
-            return Some(prime);
-        }
-    }
-    if number >= 2 {
-        Some(number)
-    } else {
-        None
+        factors.pop()
     }
 }
 
 fn main() {
-    let greatest_prime_factor = find_greatest_prime_factor(49870000);
+    let greatest_prime_factor = find_greatest_prime_factor(600851475143);
     println!("{}", greatest_prime_factor.unwrap());
 }
